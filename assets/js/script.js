e488cf11bb20a030e72dbffc6f81d86e7815015c
Himal Nimsara <himalnimsara88@gmail.com>
2024-06-16 01:01:28 +0530

    ->Responsive completed

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,7 +9,19 @@ $("#nav-skills").eq(0).on('click', ()=>{
     $('.experieenc').css({display:'none'})
     $('.assignments').css({display:'none'})
 });
+$("#mobile-skills").eq(0).on('click', ()=>{
+    $('.professionalskills').css({display:'block'})
+    $('.education-qualification').css({display:'none'})
+    $('.experieenc').css({display:'none'})
+    $('.assignments').css({display:'none'})
+});
 $("#nav-education").eq(0).on('click', ()=>{
+    $('.education-qualification').css({display:'block'})
+    $('.professionalskills').css({display:'none'})
+    $('.experieenc').css({display:'none'})
+    $('.assignments').css({display:'none'})
+});
+$("#mobile-education").eq(0).on('click', ()=>{
     $('.education-qualification').css({display:'block'})
     $('.professionalskills').css({display:'none'})
     $('.experieenc').css({display:'none'})
@@ -21,8 +33,20 @@ $("#nav-experience").eq(0).on('click', ()=>{
     $('.professionalskills').css({display:'none'})
     $('.assignments').css({display:'none'})
 });
+$("#mobile-experience").eq(0).on('click', ()=>{
+    $('.experieenc').css({display:'block'})
+    $('.education-qualification').css({display:'none'})
+    $('.professionalskills').css({display:'none'})
+    $('.assignments').css({display:'none'})
+});
 
 $("#nav-assignments").eq(0).on('click', ()=>{
+    $('.assignments').css({display:'block'})
+    $('.experieenc').css({display:'none'})
+    $('.education-qualification').css({display:'none'})
+    $('.professionalskills').css({display:'none'})
+});
+$("#mobile-assignments").eq(0).on('click', ()=>{
     $('.assignments').css({display:'block'})
     $('.experieenc').css({display:'none'})
     $('.education-qualification').css({display:'none'})
